d5856c07dcd80ea39822480965e5f3b2abf5fd5a
Theresa2013 <tab61295@gmail.com>
2022-08-03 18:47:20 -0500

    adding webpack analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
-
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -13,6 +13,9 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 };
